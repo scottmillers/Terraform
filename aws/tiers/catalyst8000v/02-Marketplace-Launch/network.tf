9ac6d8d0d2b64dd47de562e849a746538bc22d8d
Scott Miller <scott_millers@hotmail.com>
2024-01-08 17:09:32 +0000

    Align the naming of the network components(e.g. VPC) to the documentation Fixes #19

--- aws/tiers/catalyst8000v/02-Marketplace-Launch/network.tf
+++ aws/tiers/catalyst8000v/02-Marketplace-Launch/network.tf
@@ -1,65 +1,78 @@
 
-// Create the VPC for the Cisco SD-WAN
-resource "aws_vpc" "vpc_cisco8000v" {
-  cidr_block           = var.aws_vpc_cisco8000v_cidr
+//  Network VPC
+resource "aws_vpc" "network" {
+  cidr_block           = var.network_vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "${var.bucket_prefix} Cisco8000v VPC"
+    Name = "${var.bucket_prefix} Network VPC"
+  }
+}
+
+//  Production VPC
+resource "aws_vpc" "production" {
+  cidr_block           = var.production_vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.bucket_prefix} Production VPC"
   }
 }
 
 
 
-# Create a Subnets for the Cisco8000V controller and node
-resource "aws_subnet" "cisco8000v_subnet1" {
-  vpc_id                  = aws_vpc.vpc_cisco8000v.id
-  cidr_block              = var.aws_cisco8000v_subnet1_cidr
+# Subnets for the Cisco8000V controller and node
+resource "aws_subnet" "network_subneta" {
+  vpc_id                  = aws_vpc.network.id
+  cidr_block              = var.network_subneta_cidr
   availability_zone       = var.aws_az1
   map_public_ip_on_launch = true # Auto-assign public IPs to instances in this subnet
   tags = {
-    Name = "${var.bucket_prefix} Cisco8000V Subnet1"
+    Name = "${var.bucket_prefix} Cisco8000V SubnetA"
   }
 }
 
-/*
-resource "aws_subnet" "controllers_subnet2" {
-    vpc_id                    = aws_vpc.vpc_controllers.id
-    cidr_block                = var.aws_controllers_subnet2_cidr
-    availability_zone         = var.aws_controllers_az
-    tags = {
-        Name                  = "${var.bucket_prefix} SD-WAN-Controllers-Subnet-2-vpn0"
-    }
+
+# Subnets for the TIERS Production Instance
+resource "aws_subnet" "production_subneta" {
+  vpc_id                  = aws_vpc.production.id
+  cidr_block              = var.production_subneta_cidr
+  availability_zone       = var.aws_az1
+  map_public_ip_on_launch = true # Auto-assign public IPs to instances in this subnet
+  tags = {
+    Name = "${var.bucket_prefix} Production SubnetA"
+  }
 }
-*/
+
 
 # Create IGW for Internet Access:
-resource "aws_internet_gateway" "cisco8000v_igw" {
-  vpc_id = aws_vpc.vpc_cisco8000v.id
+resource "aws_internet_gateway" "network_igw" {
+  vpc_id = aws_vpc.network.id
   tags = {
-    Name = "${var.bucket_prefix} Cisco8000V-igw"
+    Name = "${var.bucket_prefix} Network igw"
   }
 }
 
 
 # Create route tables and default route pointing to IGW in VPN512 and VPN0:
 resource "aws_route_table" "cisco8000v_rt" {
-  vpc_id = aws_vpc.vpc_cisco8000v.id
+  vpc_id = aws_vpc.network.id
   route {
     //
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.cisco8000v_igw.id
+    gateway_id = aws_internet_gateway.network_igw.id
   }
 
   tags = {
-    Name = "${var.bucket_prefix} Cisco8000V-rt"
+    Name = "${var.bucket_prefix} Cisco8000V rt"
   }
 }
 
 # Create security group:
 resource "aws_security_group" "cisco8000v_sg" {
-  vpc_id = aws_vpc.vpc_cisco8000v.id
+  vpc_id = aws_vpc.network.id
 
   egress {
     from_port   = 0
@@ -120,7 +133,7 @@ resource "aws_security_group" "cisco8000v_sg" {
 # Associate CRT and Subnet for VPN512 and VPN0:
 
 resource "aws_route_table_association" "cisco8000v_rta_subnet1" {
-  subnet_id      = aws_subnet.cisco8000v_subnet1.id
+  subnet_id      = aws_subnet.network_subneta.id
   route_table_id = aws_route_table.cisco8000v_rt.id
 }
 /*
--- aws/tiers/catalyst8000v/02-Marketplace-Launch/variables.tf
+++ aws/tiers/catalyst8000v/02-Marketplace-Launch/variables.tf
@@ -9,22 +9,35 @@ variable "aws_region" {
 }
 
 
-variable "aws_vpc_cisco8000v_cidr" {
-  default = "10.0.0.0/16"
-}
-
-variable "aws_cisco8000v_subnet1_cidr" {
-  default = "10.0.1.0/24"
-}
-
 variable "aws_az1" {
   default = "us-east-2a"
 }
 
 
 
-variable "aws_tiers_subnet_cidr" {
-  default = "10.0.2.0/24"
+variable "network_vpc_cidr" {
+  default = "100.101.0.0/24" #256 addresses for the network
+}
+
+variable "network_subneta_cidr" {
+  default = "100.101.0.0/25" # 128 ip addresses. Range is 100.101.0.0 to 100.101.0.127
+}
+
+variable "network_subnetb_cidr" {
+  default = "100.101.0.128/25" # 128 ip addresses. Range is 100.101.0.129 to 100.101.0.254
+}
+
+
+variable "production_vpc_cidr" {
+  default = "10.0.0.0/24" #256 addresses for the production
+}
+
+variable "production_subneta_cidr" {
+  default = "10.0.0.0/25"
+}
+
+variable "production_subnetb_cidr" {
+  default = "10.0.0.128/25"
 }
 
 
@@ -42,37 +55,31 @@ variable "ec2_username" {
 }
 
 
-#variable aws_ami_id_cisco8000vbyol {
-# default = "ami-0f1ab0f93d85e0877"  # Cisco-C8K-17.13.01a in us-east-1
-#}
-
+variable "aws_ami_id_awslinux2023" {
+  default = "ami-0ee4f2271a4df2d7d" # AWS Linux 2023 in us-east-2 
+}
 variable "aws_ami_id_cisco8000v_byol" {
   default = "ami-073d0f81e96822a78" # Cisco-C8K-17.13.01a in us-east-2
 }
 
+#variable aws_ami_id_cisco8000vbyol {
+# default = "ami-0f1ab0f93d85e0877"  # Cisco-C8K-17.13.01a in us-east-1
+#}
 
-variable "aws_instance_type_cisco8000v_controller" {
+
+variable "controller_vpn0_privateip_address" {
+  default = "100.101.0.4"
+}
+variable "controller_instance_type" {
   default = "t3.medium"
 }
 
 
-variable "cisco8000v_controller_nic1_ip_address" {
-  default = "10.0.1.11"
-}
 
 variable "aws_instance_type_cisco8000v_node" {
   default = "t3.medium"
 }
 
-
-variable "aws_ami_id_awslinux2023" {
-  default = "ami-0ee4f2271a4df2d7d" # AWS Linux for the TIERS web server
-}
-
-
-#variable  "aws_sdwan_subnet2_private_ip" {
-#  default = "10.0.2.11"
-#}
 
 variable "ssh_allow_cidr" { # allow ssh from anywhere. needs to be modified!
   default = "0.0.0.0/0"
